2a2f34c438371a0f8cd1f72ac77ced783be3c1de
Alejandro Sandoval <asandovalster@gmail.com>
2021-08-08 16:18:30 -0400

    background_color

--- packages/crmweb/src/containers/Login/Login.tsx
+++ packages/crmweb/src/containers/Login/Login.tsx
@@ -1,109 +1,118 @@
-import React, { useContext } from 'react';
-import { Redirect, useHistory, useLocation } from 'react-router-dom';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-import { AuthContext } from 'context/auth';
-import {
-  FormFields,
-  FormLabel,
-  FormTitle,
-  Error,
-} from 'components/FormFields/FormFields';
+import { useContext, useState } from 'react';
 import { Wrapper, FormWrapper, LogoImage, LogoWrapper } from './Login.style';
-import Input from 'components/Input/Input';
-import Button from 'components/Button/Button';
+import { gql, useLazyQuery } from '@apollo/client';
+import { useHistory, useLocation } from 'react-router';
 import Logoimage from 'assets/image/tuecommerce.png';
+import '../../settings/constants';
+import { DASHBOARD } from '../../settings/constants';
+import { AuthContext } from 'context/auth';
+import { FormFields, FormTitle } from 'components/FormFields/FormFields';
+import { Alert } from 'react-bootstrap';
 
-const initialValues = {
-  username: '',
-  password: '',
-};
-
-const getLoginValidationSchema = () => {
-  return Yup.object().shape({
-    username: Yup.string().required('Username is Required!'),
-    password: Yup.string().required('Password is Required!'),
-  });
-};
-
-const MyInput = ({ field, form, ...props }) => {
-  return <Input {...field} {...props} />;
-};
+const GET_SUSCRIPTOR = gql`
+	query GETSUSCRIPTOR($usuario: String!,$password: String!) {
+	info_user_view(where: {usuario: {_eq: $usuario}, clave: {_eq: $password},fecha_vencimiento: {_gte: "now()"}}) {
+	  img_site_url
+	  img_user_url
+	  nombre
+	  usuario
+	  plan_suscripcion
+	  fecha_vencimiento
+	  estado
+	  clientid
+	  fecha_suscripcion
+	}
+  }
+  
+  
+`;
 
 export default function Login() {
-  let history = useHistory();
-  let location = useLocation();
-  const { authenticate, isAuthenticated } = useContext(AuthContext);
-  if (isAuthenticated) return <Redirect to={{ pathname: '/' }} />;
+	const [usuario, SetUsuario] = useState('');
+	const [password, SetPassword] = useState('');
+	const { authenticate, isAuthenticated } = useContext(AuthContext);
+	let history = useHistory();
+	let location = useLocation();
+	let { from } = (location.state as any) || { from: { pathname: DASHBOARD } };
+	const [loadAuthorInfo, { called, loading, data: authorInfo }] = useLazyQuery(
+		GET_SUSCRIPTOR, {
+		variables: { usuario, password }
+	});
 
-  let { from } = (location.state as any) || { from: { pathname: '/' } };
-  let login = ({ username, password }) => {
-    authenticate({ username, password }, () => {
-      history.replace(from);
-    });
-  };
-  return (
-    <Wrapper>
-      <FormWrapper>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={login}
-          render={({ errors, status, touched, isSubmitting }) => (
-            <Form>
-              <FormFields>
+	function handleInput(e) {
+		console.log(e.target.name);
+		console.log(e.target.value);
+		switch (e.target.name) {
+			case 'usuario':
+				SetUsuario(e.target.value);
+				break;
+			case 'password':
+				SetPassword(e.target.value);
+				break;
+		}
+	}
+	const auth = () => {
+
+		authenticate({ usuario, password }, () => {
+			console.log(usuario, password);
+			history.replace(from);
+		});
+	}
+
+	const handleMoreInfo = () => {
+		console.log('handleMoreInfo');
+		loadAuthorInfo();  // or loadAuthorInfo({ variables: { author: book.author.id } });
+	}
+
+	return (
+	  <>
+			<Wrapper>
+				<FormWrapper>
+							<FormFields>
                 <LogoWrapper>
                   <LogoImage src={Logoimage} alt="pickbazar-admin" />
                 </LogoWrapper>
-                <FormTitle>Log in to admin!</FormTitle>
+                <FormTitle>Ingreso Administración</FormTitle>
+								{
+									authorInfo! && <Alert key={1} variant={'danger'} transition={true}>
+    							Suscriptor no identificado o problemas para validar sus datos, intente nuevamente!!!
+  								</Alert>
+								}
               </FormFields>
+	 					<div className="form-group">
+							<label>Usuario</label>
+							<input type="text" name="usuario" className="form-control" onChange={handleInput} placeholder="suscriptor@demo.cl"   />
+						</div>
 
-              <FormFields>
-                <FormLabel>Username</FormLabel>
-                <Field
-                  type="email"
-                  name="username"
-                  component={MyInput}
-                  placeholder="Ex: demo@demo.com"
-                />
-                {errors.username && touched.username && (
-                  <Error>{errors.username}</Error>
-                )}
-              </FormFields>
-              <FormFields>
-                <FormLabel>Password</FormLabel>
-                <Field
-                  type="password"
-                  name="password"
-                  component={MyInput}
-                  placeholder="Ex: demo"
-                />
-                {errors.password && touched.password && (
-                  <Error>{errors.password}</Error>
-                )}
-              </FormFields>
-              <Button
-                type="submit"
-                disabled={isSubmitting}
-                overrides={{
-                  BaseButton: {
-                    style: ({ $theme }) => ({
-                      width: '100%',
-                      marginLeft: 'auto',
-                      borderTopLeftRadius: '3px',
-                      borderTopRightRadius: '3px',
-                      borderBottomLeftRadius: '3px',
-                      borderBottomRightRadius: '3px',
-                    }),
-                  },
-                }}
-              >
-                Submit
-              </Button>
-            </Form>
-          )}
-          validationSchema={getLoginValidationSchema}
-        />
-      </FormWrapper>
-    </Wrapper>
-  );
+						<div className="form-group">
+							<label>Contraseña</label>
+							<input type="password" name="password" className="form-control" onChange={handleInput} placeholder="Ingrese su clave"   />
+						</div>
+
+						<div className="form-group">
+							<div className="custom-control custom-checkbox">
+								<input type="checkbox" className="custom-control-input" id="customCheck1" />
+								<label className="custom-control-label" htmlFor="customCheck1">Recordarme</label>
+							</div>
+						</div>
+
+						<button onClick={handleMoreInfo} className="btn btn-success btn-block">Ingresar</button>
+						{/* {called && loading} */}
+						{authorInfo && authorInfo.info_user_view && authorInfo.info_user_view.map(item => {
+							console.log(1);
+							sessionStorage.setItem('pickbazar_token', `${usuario}.${password}`);			
+							console.log(2);
+							sessionStorage.setItem('clientid', item.clientid);
+							sessionStorage.setItem('infoUser', JSON.stringify(item));
+							console.log(3);
+							auth()
+						})
+						}
+						<p className="forgot-password text-right">
+							Se me olvido la  <a href="#">contraseña?</a>
+						</p>
+					</FormWrapper>
+			</Wrapper>
+        </>
+	)
 }
